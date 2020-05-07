f683b63bfce21e2bca5f752349731fa58b17c941
shubhanshu.rastogi <shubhanshu.rastogi@klingelnberg.com>
2020-05-07 23:33:59 +0530

    Two Sum in C#

---
+++ arrays/C#/LeetCode-Arrays/LeetCode-Arrays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeetCode_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ arrays/C#/LeetCode-Arrays/LeetCode-Arrays/TwoSum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode_Arrays
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var numbersindex = new Dictionary<int, int>();
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if (numbersindex.ContainsKey(target - nums[i]))
+                    return new int[] { numbersindex[target - nums[i]], i };
+                else
+                    numbersindex[nums[i]] = i;
+            }
+            return null;
+
+        }
+    }
+}
