0e8886ed55636ee20f2de278480d85261930d65c
shubhanshu.rastogi <shubhanshu.rastogi@klingelnberg.com>
2020-05-08 23:03:42 +0530

    Non Duplicates Solution and removed Program class

---
+++ arrays/C#/LeetCode-Arrays/LeetCode-Arrays/NonDuplicates.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode_Arrays
+{
+    class NonDuplicates
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            var nonduplicates = new HashSet<int>();
+            foreach (var i in nums)
+            {
+                if (!nonduplicates.Contains(i))
+                    nonduplicates.Add(i);
+                else
+                    return true;
+            }
+            return false;
+        }
+    }
+}
