65e2f05530ef74667fec5e3b4c4f5032e2a7728f
shubhanshu.rastogi <shubhanshu.rastogi@klingelnberg.com>
2020-05-08 19:04:52 +0530

    Buy and Sell in C#

---
+++ arrays/C#/LeetCode-Arrays/LeetCode-Arrays/BuyAndSellStock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode_Arrays
+{
+    public class MaxProfitArray
+    {
+        public int MaxProfit(int[] prices)
+        {
+            var minValue = Int32.MaxValue;
+            int maxProfit = 0;
+            for(int i =0; i<prices.Length; i++)
+            {
+                if (prices[i] < minValue)
+                    minValue = prices[i];
+                else if (prices[i] - minValue > maxProfit)
+                    maxProfit = prices[i] - minValue;
+            }
+            return maxProfit;
+        }
+    }
+}
