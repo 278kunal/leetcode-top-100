b5407cfcec53c54acd6113eda75d000eb554923b
Harshit Srivastava <harshit12893@live.com>
2020-05-08 13:11:27 +0530

    best-time-to-buy-and-sell-stock
     leetcode- Harshit

---
+++ arrays/best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit =0;
+        
+        if(prices.length <2)
+            return maxProfit;
+        int minPrice=prices[0];
+        for(int i=1;i<prices.length;i++) {
+                int currProfit=(prices[i]-minPrice);
+                maxProfit=Math.max(maxProfit,currProfit);
+            minPrice=Math.min(minPrice,prices[i]);
+        }
+        return maxProfit;
+    }
+}
