c83a5bc4f7b2d63ef9bffbc9730f4967628e8cb4
shubhanshu.rastogi <shubhanshu.rastogi@klingelnberg.com>
2020-05-10 13:59:10 +0530

    Minimum In Rotated Sub Array - C#

---
+++ arrays/C#/LeetCode-Arrays/LeetCode-Arrays/MinimumInRotatedSubArray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode_Arrays
+{
+    //https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+    class MinimumInRotatedSubArray
+    {
+        public int FindMin(int[] nums)
+        {
+            var pos = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] < nums[i - 1])
+                {
+                    pos = i;
+                    break;
+                }
+            }
+            return nums[pos];
+        }
+    }
+}
